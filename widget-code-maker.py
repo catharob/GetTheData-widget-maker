31978613c23aa22bbfb487e61ee0fc07ad816b0c
Catherine Roberts <catharob@gmail.com>
2015-08-23 11:16:45 -0400

    added handling of multiple datasets

--- widget-code-maker.py
+++ widget-code-maker.py
@@ -1,38 +1,45 @@
 import bs4
 import requests
 
-def makeCode(folder, sheet):
+def makeCode(folder):
     x = requests.get(folder)
     x.raise_for_status()
     gitSoup = bs4.BeautifulSoup(x.text)
     files = gitSoup.select('.js-directory-link') #get tag with URL for each file
     
-    urls = []
-    for f in files:
-        urls.append(f.get('href')) #put urls into list
-   
-    for u in urls:
-        if "README.md" in u:
-            urls.remove(u) #get README out of list
-            
+    urls = [f.get('href') for f in files if 'README.md' not in f.get('href')] #put urls in list without readme filr
+
     halfUrls = []
     for v in urls:
         if "/InsideEnergy/Data-for-stories/blob/master" in v:
             w = v.replace("/InsideEnergy/Data-for-stories/blob/master", "")
             halfUrls.append(w) #strip extra stuff off front of url
-    csvFile = halfUrls[0]
-    codeHasCsv = "http://rawgit.com/insideenergy/Data-for-stories/master%s" % csvFile
-    xlsFile = halfUrls[1]
-    codeHasXls = "http://rawgit.com/insideenergy/Data-for-stories/master%s" % xlsFile
 
-    #now concatonate the code together
-    widgetCode = "<small><strong> Get the data: <a href='" + codeHasCsv + "'>CSV</a> | <a href='" + codeHasXls + "'>XLS</a> | <a href='" + sheet + "' target='_blank'>Google Sheets</a> | Source and notes: <a href='" + folder + "'>Github</a> </strong></small>"       
-    print widgetCode
-
+    justFolders = []
+    for x in halfUrls:
+        if ".csv" in x:
+            y = x.replace(".csv", "")
+            justFolders.append(y)
+        if ".xlsx" in x:
+            z = x.replace(".xlsx", "")
+            justFolders.append(z) #gets file extensions off 
+    
+    noDuplicates = []
+    for z in justFolders:
+        if z not in noDuplicates:
+            noDuplicates.append(z) #gets rid of duplicates
+    
+    #now concatonate a code for each folder name, and ask for corresponding Google Sheets URL
+    for i in noDuplicates:
+        print "Enter the Google Sheets URL for public viewing that corresponds with " + i
+        mySheet = raw_input()
+        print "~~~~~~~~~~Widget code for " + i + "~~~~~~~~~~"
+        print
+        print '<small><strong> Get the data: <a href="http://rawgit.com/insideenergy/Data-for-stories/master' + i + '.csv">CSV</a> | <a href="http://rawgit.com/insideenergy/Data-for-stories/master' + i + '.xlsx">XLS</a> | <a href="' + mySheet + '" target="_blank">Google Sheets</a> | Source and notes: <a href="' + folder + '">Github</a> </strong></small>'
+        print
+        
+        
 print 'Make the "Get the Data" widget code.'
 print "Enter GitHub ULR of your new folder inside 'Data-for-stories':"
 myFolder = raw_input()
-print "Enter Google Sheets URL for public viewing:"
-mySheet = raw_input()
-print "~~~~~~~~~~Widget Code - Paste this below your chart~~~~~~~~~~"
-makeCode(myFolder, mySheet)
+makeCode(myFolder)
